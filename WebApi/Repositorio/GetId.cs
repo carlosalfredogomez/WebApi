bd1dcbea77bfc85c2b58ef9baa4aab86a3980608
carlos.alfredo.gomez <carlos.alfredo.gomez@gmail.com>
2023-02-14 19:03:50 -0300

    cuarta

--- WebApi/Repositorio/GetId.cs
+++ WebApi/Repositorio/GetId.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 
-namespace ProyectoFinalWebAPI
+namespace WebApi
 {
     public static class GetId
     {
--- WebApi/Repositorio/ManejadorUsuario.cs
+++ WebApi/Repositorio/ManejadorUsuario.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProyectoFinalWebAPI
+namespace WebApi
 {
     public class ManejadorUsuario
     {
